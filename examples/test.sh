b692492ab41faf1f04bebdeb9d16ed035c04d138
Omer Mersin <omermersin02@gmail.com>
2025-07-07 13:56:57 +0200

    build(samples): add jsoncpp & X extensions; point compiler to /usr/local/include

--- examples/test.sh
+++ examples/test.sh
@@ -25,16 +25,19 @@ DEEPSTREAM_LIB="/opt/nvidia/deepstream/deepstream/lib"
 
 # Compile the specified source file
 g++ "$FILE_NAME.cpp" -o "$FILE_NAME" \
+  -I/usr/local/include \
   $(pkg-config --cflags --libs opencv4) \
   $(pkg-config --cflags --libs gstreamer-1.0 gstreamer-rtsp-server-1.0 gstreamer-app-1.0 || echo "-L/usr/lib/aarch64-linux-gnu -lgstreamer-1.0 -lgstrtspserver-1.0 -lgstapp-1.0") \
   $(pkg-config --cflags --libs glib-2.0 gobject-2.0) \
   $(pkg-config --cflags --libs libcurl || echo "-lcurl") \
+  $(pkg-config --cflags --libs jsoncpp) \
   -I"$DEEPSTREAM_INCLUDE" -L"$DEEPSTREAM_LIB" -lnvds_meta \
   -L/usr/local/lib \
   -lvideo-stab -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_video -lopencv_videoio \
   -lopencv_imgcodecs -lopencv_cudaoptflow -lopencv_cudawarping -lopencv_calib3d \
   -lopencv_cudaimgproc -lopencv_cudafilters -lopencv_cudaarithm -lopencv_photo -lopencv_features2d \
-  -lgstreamer-1.0 -lgstrtspserver-1.0 -lgstapp-1.0 -lgobject-2.0 -lglib-2.0 -pthread -lX11 \
+  -lgstreamer-1.0 -lgstrtspserver-1.0 -lgstapp-1.0 -lgobject-2.0 -lglib-2.0 -pthread \
+  -lX11 -lXext -lXfixes -lXrender -lXrandr -lXi -lXcomposite -lXcursor -lXdamage \
   -lnvdsgst_meta -lnvds_meta -lnvds_utils
 
 
